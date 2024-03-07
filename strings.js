46086078b408219f19643235846239f3c4913c9e
Jemimah Nthenya <jemimahogonda@gmail.com>
2024-03-07 11:36:02 +0300

    add files

---
+++ strings.js
@@ -0,0 +1,32 @@
+let a ="wonderful";
+console.log(a.toUpperCase());
+
+let b ="amazing";
+console.log(b.toLowerCase());
+
+let c ="UndERneath";
+console.log(c.toLowerCase());
+
+let d ="A wonderful world";
+console.log(d.Titlecase());
+//question5
+
+let sentence="The quick brown fox jumps over the lazy dog" 
+let e="quick"
+let f="brown"
+console.log((sentence.match(/quick/)).length);
+console.log((sentence.match(/brown)).length);
+//question3
+
+const string="The pupils are reading in the library"
+let g=string1.include("are")
+console.log(g)
+const string2="The pupils are reading in the library"
+let y=string.includes("sitting")
+console.log(y)
+//question4
+
+let strng="extravaganza"
+let subst=strng.substring(8,)
+console.log(subst);
+//question2
